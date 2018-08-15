2758d2f2afce15e0950d73ac8f77c85091354615
frankliuyh <frankliuyh@qq.com>
2018-08-15 11:44:37 +0800

    get wiki image using SDWebImage Pods

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -11,6 +11,7 @@ import CoreML
 import Vision
 import Alamofire
 import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -34,7 +35,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
             detect(image: ciimage)
-            imageView.image = userPickedImage
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
@@ -66,19 +66,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let parameters : [String:String] = [
             "format" : "json",
             "action" : "query",
-            "prop" : "extracts",
+            "prop" : "extracts|pageimages",
             "exintro" : "",
             "explaintext" : "",
             "titles" : flowerName,
             "indexpageids" : "",
             "redirects" : "1",
+            "pithumbsize" : "500"
             ]
         Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
             if response.result.isSuccess {
                 let flowerJSON : JSON = JSON(response.result.value!)
                 let pageid = flowerJSON["query"]["pageids"][0].stringValue
                 let flowerDescirption = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
                 
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
                 self.label.text = flowerDescirption
             }
         }
